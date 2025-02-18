a9bc7a02a16588e765e98facd37e437763d35be8
LeraChernyakova <2215391@mail.ru>
2025-02-18 11:37:28 +0300

    Add string resource for main client screen

--- app/src/main/java/ru/moevm/printhubapp/presentation/client/MainClientScreen.kt
+++ app/src/main/java/ru/moevm/printhubapp/presentation/client/MainClientScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,7 +48,7 @@ fun MainClientScreen() {
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
-                        text = "PrintHub",
+                        text = stringResource(R.string.main_title),
                         fontSize = 24.sp,
                         fontWeight = FontWeight.SemiBold,
                         color = AppTheme.colors.black9
@@ -82,7 +83,7 @@ fun MainClientScreen() {
                             onClick = { TODO() },
                             label = {
                                 Text(
-                                    text = item.titleResId,
+                                    text = stringResource(item.titleResId),
                                     fontSize = 20.sp
                                 )
                             },
--- app/src/main/java/ru/moevm/printhubapp/presentation/client/NavigationItem.kt
+++ app/src/main/java/ru/moevm/printhubapp/presentation/client/NavigationItem.kt
@@ -1,14 +1,16 @@
 package ru.moevm.printhubapp.presentation.client
 
+import ru.moevm.printhubapp.R
+
 sealed class NavigationItem(
-    val titleResId: String
+    val titleResId: Int
 ) {
 
     object Home : NavigationItem (
-        titleResId =  "Главная"
+        titleResId =  R.string.navigation_item_home
     )
 
     object Profile : NavigationItem (
-        titleResId =  "Профиль"
+        titleResId =  R.string.navigation_item_profile
     )
 }
