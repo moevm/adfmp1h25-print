c6db714a32b49943d514e7d24d3b2d8ef2b5ee1a
DaniiLBez <dvbezzubov@stud.eltech.ru>
2025-02-23 23:23:28 +0300

    add dependencies for di

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.services)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
