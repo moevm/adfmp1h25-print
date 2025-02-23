032d82b9104ae075e15aa8d77eb9bcf6a0013b37
DaniiLBez <dvbezzubov@stud.eltech.ru>
2025-02-23 23:16:18 +0300

    add dependencies for firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -58,4 +59,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(libs.androidx.navigation.compose)
+
+    implementation(platform(libs.firebase))
+    implementation(libs.firebase.auth)
 }
